54810e573ff1ac4be645bc9c0aad1b9a35d09928
D0LLYNH020 <00001114097809sp@al.educacao.sp.gov.br>
2024-12-06 15:57:01 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Mundo',
+    'Qual o País conhecido por comidas como a lasaha e a pizza?',
+    'É a Itália'
+)
+
+criaCartao(
+    'Matemática',
+    'Quanto é 23 + 32?',
+    '55'
+)
+
+criaCartao(
+    'Música',
+    'Qual artista gravou a música Billie Jean?',
+    'Michael Jackson'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual país faz fronteira com Portugal?',
+    'A Espanha'
+)
